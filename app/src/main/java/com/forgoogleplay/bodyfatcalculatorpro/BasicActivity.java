0b5d02ef5f119a1cdb818b642e000224cb597bc3
Пламен Господинов <plamko3@abv.bg>
2021-11-20 23:58:47 +0200

    Advancing more with the design,getting close to the logic.

---
+++ app/src/main/java/com/forgoogleplay/bodyfatcalculatorpro/BasicActivity.java
@@ -0,0 +1,16 @@
+package com.forgoogleplay.bodyfatcalculatorpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class BasicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.basic_activity);
+    }
+}
--- app/src/main/java/com/forgoogleplay/bodyfatcalculatorpro/MainActivity.java
+++ app/src/main/java/com/forgoogleplay/bodyfatcalculatorpro/MainActivity.java
@@ -2,7 +2,9 @@ package com.forgoogleplay.bodyfatcalculatorpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void goToBasicActivity(View view) {
+        Intent intent = new Intent(this, BasicActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToProActivity(View view) {
+        Intent intent = new Intent(this, ProActivity.class);
+        startActivity(intent);
+    }
+
 }
